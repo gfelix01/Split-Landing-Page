2ab587430799ee796d33aecb1122013eb576d174
Gabriel Felix <gabriel_arturo01@hotmail.com>
2023-09-13 12:57:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const left = document.querySelector('.left')
+const rigth = document.querySelector('.rigth')
+const container = document.querySelector('.container')
+
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove
+    ('hover-left'))
+
+
+rigth.addEventListener('mouseenter', () => container.classList.add
+    ('hover-right'))
+rigth.addEventListener('mouseleave', () => container.classList.remove
+    ('hover-right'))
+
+
